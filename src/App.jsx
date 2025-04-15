c52df266dcc7e422f83bdc5494b16cce684c551a
utof <bondar030@mail.ru>
2025-04-16 01:12:44 +0300

    simplifcation cuz idk wtf is going on

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,38 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
-
-const FullScreenDiv = styled.div`
-  width: 100vw;
-  height: 100vh;
-  margin: 0;
+const TextAreaWrapper = styled.div`
   padding: 0;
-  box-sizing: border-box;
-  overflow: hidden;
-  display: flex;
-  align-items: stretch;
-  justify-content: stretch;
-`;
-
-const EdgeToEdgeText = styled.div`
-  width: 100vw;
-  height: 100vh;
+  /* text-align: justify; */
   margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-size: 2rem;
-  line-height: 1.2;
-  word-break: break-all;
-  white-space: pre-wrap;
-  /* Force wrap after N characters (e.g., 40ch) */
-  max-width: 100vw;
-  max-height: 100vh;
-  display: flex;
-  align-items: stretch;
-  justify-content: stretch;
+  /* padding: 0; */
+
+  &:after {
+    content: "";
+    /* display: inline-block; */
+    /* width: 100%; */
+    color: #ff1818;
+    /* pointer-events: none; */
+  }
 `;
 
 export default function App() {
-  const [text, setText] = useState("AAAAAAAAAAAAAA");
+  const [text, setText] = useState("ASADDSADASDASDA");
 
   return (
-    <FullScreenDiv>
-      <EdgeToEdgeText>
-        <textarea
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          style={{
-            width: "100%",
-            height: "100%",
-            fontSize: "2rem",
-            lineHeight: 1.2,
-            border: "none",
-            resize: "none",
-            background: "transparent",
-            color: "inherit",
-            fontFamily: "inherit",
-            outline: "none",
-            boxSizing: "border-box",
-          }}
-        />
-      </EdgeToEdgeText>
-    </FullScreenDiv>
+    <TextAreaWrapper>
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        style={{
+          overflow: "hidden",
+          width: "100%",
+          height: "100%",
+          lineHeight: 1.2,
+          outline: "none",
+          resize: "none",
+          // boxSizing: "border-box",
+        }}
+      />
+    </TextAreaWrapper>
   );
 }
